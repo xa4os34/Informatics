a031658d914b2c8ea080c0ad3e35f1339611e66b
xa4os34 <mathyushag8907@gmail.com>
2023-12-21 20:23:29 +0500

    added v1

---
+++ Labs/11-13/v1/AreaCalculator.pas
@@ -0,0 +1,135 @@
+const r = 35.0;
+const a = 35.0;
+const b = 25.0;
+const circleX = 2;
+const circleY = -4.057;
+const ellipseX = -18;
+const ellipseY = -5.711;
+const circleXProjection = -33;
+const k = -0.831243320779365;
+const c = -13.36187068043;
+const circleEllipseIntersectionX = -20.776;
+const ellipseLineXIntersection = 16.075;
+const circleLineXIntersection = 32.290;
+const formatA = ':000.0000';
+const formatB = ':000.0000';
+
+type CalculatingAreaFunction = function(
+    func : Func<Double, Double>;
+    lowerBound, upperBound : Double; 
+    iterations : Integer) : Double;
+
+function CalculateAreaUnderCurveByRect(
+    func : Func<Double, Double>;
+    lowerBound, upperBound : Double;
+    iterations : Integer)
+    : Double;
+begin
+    var area : real = 0;
+    var dx := (upperBound - lowerBound) / iterations;
+    for var i := 0 to iterations - 1 do begin
+        area += dx * func(dx * i + lowerBound)
+    end;
+
+    Result := abs(area);
+end;
+
+function CalculateAreaUnderCurveByTrapezoid(
+    func : Func<Double, Double>;
+    lowerBound, upperBound : Double;
+    iterations : Integer)
+    : Double;
+begin
+    var area : real = 0;
+    var previouseValue := func(0);
+    var dx := (upperBound - lowerBound) / iterations;
+
+    for var i := 1 to iterations - 2 do begin
+        area += func(dx * i + lowerBound);
+    end;
+
+    area += (func(lowerBound) + func(upperBound)) / 2;
+    Result := abs(area * dx);
+end;
+
+function GetAArea(
+    iterations : Integer; 
+    GetAreaUnderCurve : CalculatingAreaFunction) 
+    : Double;
+begin
+    var aArea := GetAreaUnderCurve(
+        x -> circleY - sqrt(r**2 - (x - circleX)**2),
+        CircleXProjection, CircleEllipseIntersectionX,
+        iterations);
+
+    aArea += GetAreaUnderCurve(
+        x -> k * x + c,
+        CircleEllipseIntersectionX, 0,
+        iterations);
+
+    Result := aArea;
+end;
+
+function GetBArea(
+    iterations : Integer; 
+    getAreaUnderCurve : CalculatingAreaFunction)
+    : Double;
+begin
+    var bArea := getAreaUnderCurve(
+        x -> circleY + sqrt(r**2 - (x - circleX)**2),
+        0, circleLineXIntersection,
+        iterations);
+
+    bArea -= k * (ellipseLineXIntersection + circleLineXIntersection) / 2 + c;
+
+    bArea -= getAreaUnderCurve(
+        x -> -(b * sqrt(a**2 - (x - ellipseX)**2) - a * ellipseY) / a, 
+        0, ellipseLineXIntersection, 
+        iterations); 
+
+    Result := bArea;
+end;
+
+begin 
+    var tableBuilder := new StringBuilder();
+
+    tableBuilder
+        .AppendLine('┏━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━┓')
+        .AppendLine('┃  Itera-  ┃        Area A       ┃        Area B       ┃')
+        .AppendLine('┃  tions   ┣━━━━━━━━━━┳━━━━━━━━━━╋━━━━━━━━━━┳━━━━━━━━━━┫')
+        .AppendLine('┃          ┃Rectangle ┃Trapezoid ┃Rectangle ┃Trapezoid ┃');
+    
+    for var i := 1 to ParamCount do begin
+        var iterations : Integer;
+
+        if not Integer.TryParse(ParamStr(i), iterations) then
+            continue;
+
+        var aAreaByRect := GetAArea(
+            iterations,
+            CalculateAreaUnderCurveByRect); 
+
+        var aAreaByTrapezoid := GetAArea(
+            iterations,
+            CalculateAreaUnderCurveByTrapezoid); 
+    
+        var bAreaByRect := GetBArea(
+            iterations,
+            CalculateAreaUnderCurveByRect);
+
+        var bAreaByTrapezoid := GetBArea(
+            iterations,
+            CalculateAreaUnderCurveByTrapezoid); 
+
+        tableBuilder.AppendLine('┣━━━━━━━━━━╋━━━━━━━━━━╋━━━━━━━━━━╋━━━━━━━━━━╋━━━━━━━━━━┫')
+            .AppendLine(string.Format(
+                '┃ {0,-8} ┃ {1'+formatA+'} ┃ {2'+formatA+'} ┃ {3'+formatB+'} ┃ {4'+formatB+'} ┃',
+                iterations, aAreaByRect, aAreaByTrapezoid, bAreaByRect, bAreaByTrapezoid));
+
+        iterations *= 10;
+    end;
+
+    tableBuilder.AppendLine('┗━━━━━━━━━━┻━━━━━━━━━━┻━━━━━━━━━━┻━━━━━━━━━━┻━━━━━━━━━━┛');
+
+    writeln(tableBuilder.ToString());
+end.
---
+++ Labs/11-13/v1/PointsCalculatingCuted.pas
@@ -0,0 +1,59 @@
+const r = 35.0;
+const a = 35.0;
+const b = 25.0;
+const circleXProjection = -33.0; 
+const circleYIntersection = -39.0; 
+const ellipseYIntersectin = -30.0; 
+const ellipseXProjection = -43.0; 
+
+function GetSecondCoordinateOfCircle(coordinate, radius : real) : real; begin
+    Result := sqrt(sqr(radius) - sqr(coordinate));
+end;
+
+function GetSecondCoordinateOfEllipse(coordinate, radius1, radius2 : real) : real; begin
+    Result := radius1 * sqrt(1 - sqr(coordinate / radius2));
+end;
+
+begin 
+    var dx := 0.001;
+    while true do begin
+        print('введите dx: ');
+        read(dx);
+    
+        var circleX := circleXProjection + r; 
+        var circleY := circleYIntersection + GetSecondCoordinateOfCircle(-circleX, r); 
+    
+        var ellipseX := ellipseXProjection + b; 
+        var ellipseY := ellipseYIntersectin + GetSecondCoordinateOfEllipse(-ellipseX, a, b); 
+    
+        var ellipseLineXIntersection := ellipseX + GetSecondCoordinateOfEllipse(-ellipseY, a, b); 
+        
+        var circleEllipseIntersectionX := circleXProjection;
+        var circleEllipseIntersectionY := circleY - GetSecondCoordinateOfCircle(circleEllipseIntersectionX - circleX, r);
+    
+        while sqr((circleEllipseIntersectionX - ellipseX) / a) + sqr((circleEllipseIntersectionY - ellipseY) / b) < 1 do begin 
+            circleEllipseIntersectionX += dx;
+            circleEllipseIntersectionY := circleY - GetSecondCoordinateOfCircle(circleEllipseIntersectionX - circleX, r);
+    	end;
+        
+    	var k := -circleEllipseIntersectionY / (ellipseLineXIntersection - circleEllipseIntersectionX); 
+    	var c := - k * ellipseLineXIntersection;
+    
+        var circleLineIntersectionX := (sqrt(-circleY**2 + (2 * k * circleX +  2 * c) * circleY - k**2 * circleX**2 - 2 * c * k * circleX + R**2 * k**2 - c**2 + R**2) + k * circleY + circleX - c * k)/(k**2 + 1);
+        var circleLineIntersectionY := k * circleLineIntersectionX + c;
+    
+        writelnformat('dx = {0}', dx);
+    
+        writeln('┏━━━━━━━━┳━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━┓');
+        writeln('┃  name  ┃ Analytical Method ┃Computation Method ┃');
+        writeln('┃   of   ┣━━━━━━━━━┳━━━━━━━━━╋━━━━━━━━━┳━━━━━━━━━┫');
+        writeln('┃  point ┃    X    ┃    Y    ┃    X    ┃    Y    ┃');
+        writeln('┣━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫');
+        writelnformat('┃   A    ┃ -07.000 ┃  20.000 ┃ {0: 00.000;-00.000} ┃ {1: 00.000;-00.000} ┃', 
+            circleEllipseIntersectionX, circleEllipseIntersectionY);
+        writeln('┣━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫');
+        writelnformat('┃   B    ┃  35.000 ┃ -10.000 ┃ {0: 00.000;-00.000} ┃ {1: 00.000;-00.000} ┃',
+            circleLineIntersectionX, circleLineIntersectionY);
+        writeln('┗━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━┛');
+    end;
+end.
